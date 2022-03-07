eedda44cfc9271ec9f03c68129f2b7803ce68ce7
Varun Barad <varun@varunbarad.com>
2022-03-07 11:58:27 +0530

    Erlang: Add day-3 exercises

--- erlang/translate.erl
+++ erlang/translate.erl
@@ -9,6 +9,9 @@ loop() ->
 		"blanca" ->
 			io:format("white~n"),
 			loop();
+		"fallecer" ->
+			io:format("die~n"),
+			exit({"The translator went caput."});
 		_ ->
 			io:format("I don't understand~n"),
 			loop()
